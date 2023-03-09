7f4e1776ec9d39bb6308b37039b519b8e4db6abb
Dhawith3 <51269738+Dhawith3@users.noreply.github.com>
2023-03-08 20:31:42 -0500

    Update and rename celsiusToFahrenheit.py to CelsiusToFahrenheit.py

--- Class 1/celsiusToFahrenheit.py
+++
@@ -1,5 +0,0 @@
-#This is my program to convert Calcius to Fahrenheit
-print("Enter temperature in Celcius: ")
-c=input()
-F=(float(c) * 9/5)+32
-print(f"The temperature in Fahrenheit is {F} degrees")
