9c004344b7dc77c446ce5fa09947324f406fd26c
Dhawith3 <51269738+Dhawith3@users.noreply.github.com>
2023-03-08 21:26:47 -0500

    Delete areaWithRadius.py

--- Class 1/areaWithRadius.py
+++
@@ -1,7 +0,0 @@
-import math
-#area of circle
-print("Enter radius of a circle")
-radius = input()
-area = math.pi * float(radius)**2
-circumfirence = 2 * math.pi * float(radius)
-print(f"Area of the circle={area}")
