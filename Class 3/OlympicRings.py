e59b39b0d91ad9c0569ae10f82da52a67cd7c8eb
Dhawith3 <51269738+Dhawith3@users.noreply.github.com>
2023-03-09 21:28:26 -0500

    Update OlympicRings.py

--- Class 3/OlympicRings.py
+++ Class 3/OlympicRings.py
@@ -1,17 +1,56 @@
 from turtle import *
 
-pensize(10)
-f = 2.5 * 50
+s = 100
+xo = -250
+xt = -140
+yo = 100
+yt = -25
+add = 220
+pensize(8)
 
-def rings( x, y):
-    penup()
-    goto(x, y)
-    pendown()
-    color("black")
-    circle(50)
+penup()
 
-rings(-200, 100)
-rings(-200 + f, 100)
-rings((-200 + f) + f, 100)
-rings(-142, 50)
-rings(-142 + f, 50)
+goto(xo, yo)
+color("blue")
+
+pendown()
+
+circle(s)
+
+penup()
+
+goto(xo + add, yo)
+color("black")
+
+pendown()
+
+circle(s)
+
+penup()
+
+goto(xo + add * 2, yo)
+color("red")
+
+pendown()
+
+circle(s)
+
+penup()
+
+goto(xt, yt)
+color("yellow")
+
+pendown()
+
+circle(s)
+
+penup()
+
+goto(xt + add, yt)
+color("green")
+
+pendown()
+
+circle(s)
+
+done()
